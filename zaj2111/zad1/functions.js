335870c83197e7c854b45a933877f7ba912310d6
Maryna Makas <mmakas614@gmail.com>
2024-11-21 08:41:29 +0100

    plik js + nowe foldery

---
+++ zaj2111/zad1/functions.js
@@ -0,0 +1,12 @@
+function isOdd(x){
+  return x % 2 !== 0;
+}
+
+function isEven(x){
+  x % 2 == 0;
+}
+
+console.log(isOdd(5)); 
+console.log(isOdd(6)); 
+console.log(isEven(5))
+console.log(isEven(6))
